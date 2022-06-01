776dadae0b294d12782ae9030d41bdeeaf2af2a6
Pierre EVEN <peven@unistra.fr>
2022-06-01 16:12:31 +0200

    Fixed webpack for icons and added navbar base

--- src/index.js
+++ src/index.js
@@ -2,34 +2,50 @@ require("../assets/stylesheets/styles.scss");
 
 import ComponentLoader, {Component} from 'component-loader-js';
 
+import logoTwitter from '/assets/icons/logo-twitter.png';
+import logoDiscord from '/assets/icons/logo-discord.png';
+import logoGithub from '/assets/icons/logo-github.png';
+import logoInstagram from '/assets/icons/logo-instagram.png';
+import logoFacebook from '/assets/icons/logo-facebook.png';
+
 class Navbar extends Component {
 
     constructor() {
         super(...arguments);
+        this.leftElements = document.createElement('div')
+        this.rightElements = document.createElement('div')
+        this.leftElements.classList.add('navbar-left')
+        this.rightElements.classList.add('navbar-right')
+        this.el.appendChild(this.leftElements)
+        this.el.appendChild(this.rightElements)
         this.el.classList.add('navbar-container')
+
         this._addNavbarButton('HOME')
         this._addNavbarButton('ABOUT')
         this._addNavbarButton('PROJECTS')
         this._addNavbarButton('CONTACT')
 
-        this._addContactButton('twitter', '', "https://twitter.com")
-        this._addContactButton('discord', '', "https://discord.com")
-        this._addContactButton('github', '', "https://github.com")
-        this._addContactButton('instagram', '', "https://instagram.com")
+        this._addContactButton('twitter', logoTwitter, "https://twitter.com/ePierre_dev")
+        this._addContactButton('discord', logoDiscord, "https://discord.gg/rkS6f29tAP")
+        this._addContactButton('github', logoGithub, "https://github.com/PierreEVEN")
+        this._addContactButton('instagram', logoInstagram, "https://www.instagram.com/pieeerre_even")
+        this._addContactButton('facebook', logoFacebook, "https://www.facebook.com/profile.php?id=100012040879211")
     }
 
     _addNavbarButton(text) {
-        this.test = document.createElement('div')
-        this.test.id = text
-        this.el.appendChild(this.test)
-        new NavbarButton(this.test)
+        const navbarContainer = document.createElement('div')
+        navbarContainer.id = text
+        this.leftElements.appendChild(navbarContainer)
+        new NavbarButton(navbarContainer)
     }
 
     _addContactButton(alias, image, link) {
-        this.test = document.createElement('div')
-        this.test.id = alias
-        this.el.appendChild(this.test)
-        new NavbarButton(this.test)
+        const contactContainer = document.createElement('div')
+        contactContainer.id = alias
+        contactContainer.image = image
+        contactContainer.link = link
+        this.rightElements.appendChild(contactContainer)
+        new ContactButton(contactContainer)
     }
 
 
@@ -59,11 +75,15 @@ class ContactButton extends Component {
     constructor() {
         super(...arguments);
         this.button = document.createElement('button')
+        this.button.onclick = () => {
+            window.open(this.el.link)
+        }
 
-        this.text = document.createElement('p')
-        this.text.innerHTML = this.el.id
+        this.image = document.createElement('img')
+        this.image.src = this.el.image
+        this.image.innerHTML = this.el.id
 
-        this.button.appendChild(this.text)
+        this.button.appendChild(this.image)
         this.el.appendChild(this.button)
     }
 
--- webpack.config.js
+++ webpack.config.js
@@ -1,19 +1,62 @@
 const webpack = require("webpack");
 const path = require("path");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin')
 
 module.exports = {
     entry: "./src/index.js",
     output: {
-      path: path.resolve(__dirname, "./public"),
-      filename: "./bundle.js"
+      filename: "./main.js",
+      path: path.resolve(__dirname, "./dist")
     },
+	devtool: 'eval-source-map',
+	devServer: {
+		hot: "only",
+	},
+	performance: {
+		maxEntrypointSize: 1024000,
+		maxAssetSize: 1024000
+	},
     module: {
-		rules: [{
-			test: /\.scss$/,
-			use: ['style-loader', 'css-loader', 'sass-loader', 'postcss-loader']
-		}]
+		rules: [
+			{
+				test: /\.scss$/,
+				use: [
+					'style-loader',
+					'css-loader',
+					'sass-loader',
+					'postcss-loader'
+				]
+			},
+			{
+				test: /\.(gif|png|jpe?g|svg)$/,
+				use: [
+					{
+						loader: 'file-loader',
+						options: {
+							name: '[path][name].[ext]',
+							outputPath: 'assets/',
+							options: {
+								publicPath: 'assets',
+							},
+						}
+					}
+				]
+			},
+			{
+				test:/\.html$/,
+				use: [
+					'html-loader'
+				]
+			}
+		]
 	},
 	plugins: [
-		require("autoprefixer")
+		require("autoprefixer"),
+		new HtmlWebpackPlugin({
+			template: './public/index.html',
+			filename: 'index.html',
+		}),
+		new FaviconsWebpackPlugin('assets/icons/favicon.png')
 	]
   }
